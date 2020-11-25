6e0ef38f0027a2b68d1dc94f8052c1fb92e5099f
Ereldev <erelde@gmail.com>
2020-11-25 14:09:53 +0100

    Project setup

---
+++ ISSRadioContact/ISSRadioContactApp.swift
@@ -0,0 +1,17 @@
+//
+//  ISSRadioContactApp.swift
+//  ISSRadioContact
+//
+//  Created by admin on 25/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct ISSRadioContactApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
